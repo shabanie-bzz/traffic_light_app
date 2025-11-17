8428b9bf4fac9ded6cbcd116fdd7ed17489ab3aa
shabanie-bzz <114402814+shabanie-bzz@users.noreply.github.com>
2025-11-17 15:55:43 +0100

    Expand traffic light test cases
    Added test cases for undefined, orange, and green traffic light states.

--- trafficlight.test.js
+++ trafficlight.test.js
@@ -8,4 +8,22 @@ describe('test suite traffic light', () => { //??
         expect(app.trafficLight("red"))//??
             .toBe("DON'T WALK!");//??
     });//??
+
+    //case undefined (out of order)
+    it(`case undefined light`, () => { //??
+        expect(app.trafficLight())//??
+            .toBe("OUT OF ORDER");//??
+    });//??
+
+    //case orange light
+    it(`case orange light`, () => { //??
+        expect(app.trafficLight("orange"))//??
+            .toBe("ATTENTION!");//??
+    });//??
+
+    //case green light
+    it(`case green light`, () => { //??
+        expect(app.trafficLight("green"))//??
+            .toBe("WALK!");//??
+    });//??
 });//??
